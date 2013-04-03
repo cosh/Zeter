6d6b1f3a042c51ba83b734201e03d9a42025bb8c
Henning Rauch <henning@rauchentwicklung.biz>
2013-04-03 05:21:17 -0700

    adds some boilerplate code

---
+++ include/zeter.h
@@ -0,0 +1,11 @@
+#ifndef _zeter_h
+#define _zeter_h
+
+
+class Zeter
+{
+public:
+  void Print();
+};
+
+#endif
---
+++ src/zeter.cpp
@@ -0,0 +1,8 @@
+#include "zeter.h"
+#include <stdio.h>
+
+void Path::Print()
+{
+	printf("Hello, World! Das ist Zeter!\n");
+
+}
