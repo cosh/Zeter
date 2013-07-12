ade73446cd82f59284363fdf995e74b5101f75ed
Henning Rauch <Henning@RauchEntwicklung.biz>
2013-07-12 21:01:22 +0200

    starting to test the collections

--- tests/t_zeter_collections.cxx
+++ tests/t_zeter_collections.cxx
@@ -8,9 +8,29 @@
 #include <thread>
 #include <chrono>
 #include "collections/lock_free_queue.h"
+#include "synchronization/lockableElement.h"
 
 TEST(test_zeter_lock_free_queue_basic) {
+	//lock_free_queue<LockableElement>* queueLe = new lock_free_queue<LockableElement>();
+	//LockableElement le;
 
+	//queueLe->push(le);
+	//std::unique_ptr<LockableElement> lePointer = queueLe->pop();
+
+	//delete(queueLe);
+
+	/*
+	lock_free_queue<int>* queueInt = new lock_free_queue<int>();
+	int value = 4;
+
+	queueInt->push(value);
+
+	std::unique_ptr<int> valueRef = queueInt->pop();
+
+	//assert(value == valueRef);
+
+	delete(queueInt);
+*/
 	return 0;
 }
 
